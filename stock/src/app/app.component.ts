da47acd2897ad856b18ff6227769329157359648
Daniel Vaz Villalobos Martins <danielvazmartins@gmail.com>
2018-09-21 17:39:54 -0300

    fazendo exemplo do livro - stock

--- stock/src/app/app.component.ts
+++ stock/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { StockService, StockInterface } from './services/stock.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- stock/src/app/app.module.ts
+++ stock/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SummaryComponent } from './components/summary/summary.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
+import { AppRoutes } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     SummaryComponent,
-    DashboardComponent
+    DashboardComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutes
   ],
   providers: [],
   bootstrap: [AppComponent]
